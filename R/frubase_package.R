2823d176dce556c6879e053c5226c31dc6865885
Pedro Jordano <jordano@ebd.csic.es>
2018-11-06 20:37:16 +0100

    Added documentation files.

---
+++ R/frubase_package.R
@@ -0,0 +1,22 @@
+“#' FRUBASE: A package including the FRUBASE dataset.
+#'
+#' A dataset of fleshy fruit traits compiled from bibliographic references and 
+#' my own work. Data are provided for more than 1000 plant species from all 
+#' around the world. The FRUBASE package accompanies: Jordano, P. 1995. Angiosperm 
+#' fleshy fruits and seed dispersers: a comparative analysis of adaptation and 
+#' constraints in plant-animal interactions. American Naturalist 145: 163-191. 
+#' 
+#' It contains a copy of the main data file used for this paper, as well as other 
+#' accompanying files.
+#'
+#' Installation.    
+#' 
+#' # devtools::install_github("pedroj/frubase")  
+#'   
+#' # library(frubase)
+#'
+#' @docType package
+#' @name frubase
+#' @author Pedro Jordano
+#' @keywords package
+NULL
