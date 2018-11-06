06084d2df96366a4d31ec4486361468b0e7366f8
Pedro Jordano <jordano@ebd.csic.es>
2018-11-06 18:14:57 +0100

    Updated README and fixed dataset file.

--- R/dataset.R
+++ R/dataset.R
@@ -10,4 +10,16 @@
 #' @name frubase
 #' @format A dataframe.
 #' @source Dataset repository \url{https://github.com/pedroj/FRUBASE}.
+#' @examples
+#' \dontrun{
+#' data(frubase)
+#' str(frubase)
+#' 
+#' # Filtering ---------------------------------------------------------------
+#' require(dplyr)
+#' 
+#' frubase_df<- tbl_df(frubase)
+#' dplyr::slice(frubase, 7:11)
+#' frubase %>% filter(gen== "Vaccinium") %>% select(species, diam) 
+#' }
 NULL
