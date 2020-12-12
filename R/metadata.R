41fd9c3da4f87cc8715d19957b9d060fb9113533
Pedro Jordano <jordano@ebd.csic.es>
2020-12-12 17:17:32 +0100

    Added metadata code.

---
+++ R/metadata.R
@@ -0,0 +1,15 @@
+# Building metadata.
+library(dataspice)
+
+create_spice()
+
+edit_creators()
+prep_access()
+edit_access()
+prep_attributes()
+edit_attributes()
+edit_biblio()
+
+write_spice()
+
+build_site()
