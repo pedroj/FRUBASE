fd21c9d6ea380642dc9dddd773baa8700932eed4
Pedro Jordano <jordano@ebd.csic.es>
2020-12-20 17:16:51 +0100

    Edited frubase.R. Lowercased vars names,

--- R/frubase.R
+++ R/frubase.R
@@ -29,42 +29,42 @@
 #'
 #' @format A data frame with variables:
 #' \describe{
-#' \item{CL}{Class}
-#' \item{SCL}{SubClass}
-#' \item{ORD}{Order}
-#' \item{FAM}{Family}
-#' \item{GEN}{Genus}
-#' \item{SP}{Species}
-#' \item{REF}{Reference number - This is my maintenance code for updates.}
-#' \item{NEWREF}{New Reference number - These are the refs numbers in the files REFS and SUMMARY.}
-#' \item{FAMLAB}{Family Label -  An 8-character label for family.}
-#' \item{GENLAB}{Genus Label -   An 8-character label for genus.}
-#' \item{SPLAB}{Species Label - An 8-character label for species.}
-#' \item{COD}{Species code -  A 5-character code for the species.}
-#' \item{DISPCAT}{Disperser type category - BIRDS, MIXED, MAMMALS.}
-#' \item{DISP}{Disperser type - Finer categorization. Not yet completed. Needs revision.}
-#' \item{MEGAFAUNA}{ Whether the fruit species is associated with dispersal of seeds by megafauna.}
-#' \item{AREA}{Geographic area - Major geographic areas of the data sources. MEurope: Mediterranean Europe (also includes Israel and Morocco. NEurope: Temperate and Northern Europe. NAmerica: North America, excl. Southern Mexico. NTAmerica: Neotropical America, incl. Southern Mexico. Africa: Africa, south of Sahara. Australasia: Australia, Malaysia, New Guinea, and whole SouthEast Asia.}
-#' \item{HABIT}{Growth habit - Tree, Shrub, Liana, Herb.}
-#' \item{FRUIT}{Fruit type - Type of fruit. Not completed yet. Needs revision.}
-#' \item{COLOR}{Fruit color - Black, Blue, Red, Orange, Yellow, Green, Brown, White.}
-#' \item{COMPCOLOR}{Whether the fruit is monocolored, bicolored, or multicolored.}
-#' \item{LENG}{Fruit length (mm)}
-#' \item{DIAM}{Fruit diameter (mm)}
-#' \item{FRFM}{Fruit fresh mass (g)}
-#' \item{PDM}{Dry mass of pulp per fruit (g)}
-#' \item{SDM}{Dry mass of seed(s) per fruit (g)}
-#' \item{SEEDS}{Number of seeds per fruit}
-#' \item{SEEDM }{Seed dry mass (g)}
-#' \item{RY}{Relative yield of pulp (PDM/FRFM*100)}
-#' \item{KJG}{Specific energy content of pulp (kJ/g)}
-#' \item{KJFR}{Total energy content per fruit (kJ)}
-#' \item{PCW}{Percent water content of fruit}
-#' \item{LIP}{Proportion of lipids (per g dry pulp)}
-#' \item{PRO}{Proportion of protein (per g dry pulp)}
-#' \item{NSC}{Proportion of non-structural carbohydrates (per g dry pulp)}
-#' \item{ASH}{Proportion of minerals (per g dry pulp)}
-#' \item{FIB}{Proportion of acid-detergent fiber (per g dry pulp)}
+#' \item{cl}{Class}
+#' \item{scl}{SubClass}
+#' \item{ord}{Order}
+#' \item{fam}{Family}
+#' \item{gen}{Genus}
+#' \item{sp}{Species}
+#' \item{ref}{Reference number - This is my maintenance code for updates.}
+#' \item{newref}{New Reference number - These are the refs numbers in the files REFS and SUMMARY.}
+#' \item{famlab}{Family Label -  An 8-character label for family.}
+#' \item{genlab}{Genus Label -   An 8-character label for genus.}
+#' \item{splab}{Species Label - An 8-character label for species.}
+#' \item{cod}{Species code -  A 5-character code for the species.}
+#' \item{dispcat}{Disperser type category - BIRDS, MIXED, MAMMALS.}
+#' \item{disp}{Disperser type - Finer categorization. Not yet completed. Needs revision.}
+#' \item{megafauna}{ Whether the fruit species is associated with dispersal of seeds by megafauna.}
+#' \item{area}{Geographic area - Major geographic areas of the data sources. MEurope: Mediterranean Europe (also includes Israel and Morocco. NEurope: Temperate and Northern Europe. NAmerica: North America, excl. Southern Mexico. NTAmerica: Neotropical America, incl. Southern Mexico. Africa: Africa, south of Sahara. Australasia: Australia, Malaysia, New Guinea, and whole SouthEast Asia.}
+#' \item{habit}{Growth habit - Tree, Shrub, Liana, Herb.}
+#' \item{fruit}{Fruit type - Type of fruit. Not completed yet. Needs revision.}
+#' \item{color}{Fruit color - Black, Blue, Red, Orange, Yellow, Green, Brown, White.}
+#' \item{compcolor}{Whether the fruit is monocolored, bicolored, or multicolored.}
+#' \item{leng}{Fruit length (mm)}
+#' \item{diam}{Fruit diameter (mm)}
+#' \item{frfm}{Fruit fresh mass (g)}
+#' \item{pdm}{Dry mass of pulp per fruit (g)}
+#' \item{sdm}{Dry mass of seed(s) per fruit (g)}
+#' \item{seeds}{Number of seeds per fruit}
+#' \item{seedm }{Seed dry mass (g)}
+#' \item{ry}{Relative yield of pulp (PDM/FRFM*100)}
+#' \item{kjg}{Specific energy content of pulp (kJ/g)}
+#' \item{kjfr}{Total energy content per fruit (kJ)}
+#' \item{pcw}{Percent water content of fruit}
+#' \item{lip}{Proportion of lipids (per g dry pulp)}
+#' \item{pro}{Proportion of protein (per g dry pulp)}
+#' \item{nsc}{Proportion of non-structural carbohydrates (per g dry pulp)}
+#' \item{ash}{Proportion of minerals (per g dry pulp)}
+#' \item{fib}{Proportion of acid-detergent fiber (per g dry pulp)}
 #' }
 #'
 #' @references Jordano, P. 1995. Angiosperm fleshy fruits and seed dispersers: a comparative analysis of adaptation and constraints in plant-animal interactions. American Naturalist 145: 163-191.
@@ -72,5 +72,12 @@
 #' @source \href{https://raw.githubusercontent.com/pedroj/frubase/master/data/frubase.tsv}{Data txt archive}
 #'
 #' @source Dataset repository \url{https://github.com/pedroj/FRUBASE}.
+#'
+#' @examples
+#' if (require("dplyr")) {
+#' frubase
+#' frubase %>% group_by(FAM) %>% summarise(fruit.diamter = mean(DIAM))
+#' }
+#'
 "frubase"
 
