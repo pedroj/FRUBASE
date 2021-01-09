91195295c141b234d801f1f1727b9ff0f6d3fbfd
Pedro Jordano <jordano@ebd.csic.es>
2021-01-09 17:35:18 +0100

    Update frubase.R

--- R/frubase.R
+++ R/frubase.R
@@ -73,4 +73,12 @@
 #'
 #' @source Dataset repository \url{https://github.com/pedroj/FRUBASE}.
 #'
+#' @examples
+#' if (require("dplyr")) {
+#' data(frubase)
+#' head(frubase)
+#' frubase %>% 
+#'            dplyr::group_by(fam) %>% 
+#'            dplyr::summarise(fruit.diamter = mean(diam, na.rm = TRUE))
+#' }
 "frubase"
