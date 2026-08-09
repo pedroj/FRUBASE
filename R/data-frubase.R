8a7f7b1b61403f480611264117fa37e8990e5524
pedrojordano <pedroj@me.com>
2026-08-09 20:56:40 +0200

    Updated frubase for lazydata. Fixed example files.

---
+++ R/data-frubase.R
@@ -0,0 +1,7 @@
+#' FRUBASE: Fleshy fruit trait database
+#'
+#' A dataset of fruit traits compiled from bibliographic references.
+#'
+#' @format A data frame with 910 rows and 33 variables.
+#' @source Compiled by Pedro Jordano.
+"frubase"
